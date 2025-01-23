5bc4684f64a1a04103e7a5113652ef122be73b5f
Markpodrazhansky <markpodrazhansky@gmail.com>
2025-01-23 17:46:25 -0500

    Supervisor can create accounts for users

--- models/Supervisor.js
+++ models/Supervisor.js
@@ -1,8 +1,29 @@
 const Account = require('./Account');
+const Manager = require('./Manager');
+const Operator = require('./Operator');
+const Clerk = require('./Clerk');
+const Maintainer = require('./Maintainer');
 
 class Supervisor extends Account {
     constructor(username, password) {
         super(username, password, 'supervisor');
+    }
+
+    createUser(username, password, role) {
+        switch (role) {
+            case 'manager':
+                return new Manager(username, password);
+            case 'operator':
+                return new Operator(username, password);
+            case 'clerk':
+                return new Clerk(username, password);
+            case 'maintainer':
+                return new Maintainer(username, password);
+            case 'supervisor':
+                return new Supervisor(username, password);
+            default:
+                throw new Error('Invalid role');
+        }
     }
 }
 
