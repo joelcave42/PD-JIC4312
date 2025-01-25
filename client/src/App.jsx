59edd25c34fa187dbb814c58e299020b6094d47e
srithan1 <srithan@gmail.com>
2025-01-25 16:03:49 -0500

    Login Page UI
    Implemented the login page, along with a sign out button once users are logged in.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,55 @@
 import { useState } from "react";
 import { FaultSubmissionForm, FaultList } from "./components";
 import { ToastContainer, toast } from 'react-toastify';
+import LoginPage from "./components/LoginPage";
 
 
 function App() {
   const [ currentUpdatePerson, setCurrentUpdatePerson ] = useState(0);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
 
-  return (
-    <div className="container-main">
-      <FaultSubmissionForm currentUpdatePerson={currentUpdatePerson} setCurrentUpdatePerson={setCurrentUpdatePerson} />
-      <FaultList currentUpdatePerson={currentUpdatePerson} setCurrentUpdatePerson={setCurrentUpdatePerson} />
-      <ToastContainer position='top-center' />
-    </div>
-  )
+  if (isLoggedIn == false) {
+    return(
+      <div className="container-main">
+        <LoginPage onLogin={handleLogin}/>
+        <ToastContainer position='top-center' />
+      </div>
+    );
+  } else {
+    return (
+      <div className="container-main" >
+        <div style={{
+          position:'absolute',
+          top:'10px',
+          right:'25px',
+        }}>
+        <button
+          style={{
+            background: '#973c12',
+            color:'white',
+            padding:'10px 20px',
+            borderRadius: '3px',
+            border:'none',
+            cursor:'pointer',
+            margin:'10px 0'
+          }}
+          onClick={handleLogout}
+        >
+          Sign Out
+        </button>
+        </div>
+        <FaultSubmissionForm />
+        <FaultList />
+        <ToastContainer position='top-center' />
+      </div>
+    );
+  }
 }
 
 
---
+++ client/src/components/LoginPage.jsx
@@ -0,0 +1,47 @@
+// LoginPage.jsx
+import React, { useState } from 'react';
+import '../styles/LoginPage.css';
+
+function LoginPage({onLogin}) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Username:', username);
+    console.log('Password:', password);
+    onLogin();
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Hello! Please log in:</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Sign in</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
