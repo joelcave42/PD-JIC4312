41f68c92e2b89c9bfb4de2e6b9403e45935eb284
Grapeshot <joncollins@gatech.edu>
2025-01-26 07:27:25 -0500

    Revert "Added functionality for signing up for a new account, can't move further until DB setup is pushed"
    This reverts commit 6942ab73926e60357cd8013aa0a48c79713b8459.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,78 +1,56 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { FaultSubmissionForm, FaultList } from "./components";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer, toast } from 'react-toastify';
 import LoginPage from "./components/LoginPage";
-import SignUpPage from "./components/SignUpPage";
+
 
 function App() {
-  const [currentUpdatePerson, setCurrentUpdatePerson] = useState(0);
+  const [ currentUpdatePerson, setCurrentUpdatePerson ] = useState(0);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
   const handleLogin = () => {
     setIsLoggedIn(true);
   };
-
   const handleLogout = () => {
     setIsLoggedIn(false);
   };
 
-  return (
-    <Router>
+  if (isLoggedIn == false) {
+    return(
       <div className="container-main">
-        <ToastContainer position="top-center" />
-
-        {/* Define Routes */}
-        <Routes>
-          {/* Login Route */}
-          {!isLoggedIn && (
-            <Route
-              path="/"
-              element={<LoginPage onLogin={handleLogin} />}
-            />
-          )}
-
-          {/* Sign-Up Route */}
-          <Route path="/SignUpPage" element={<SignUpPage />} />
-
-          {/* Main Application Route */}
-          {isLoggedIn && (
-            <Route
-              path="/"
-              element={
-                <>
-                  <div
-                    style={{
-                      position: "absolute",
-                      top: "10px",
-                      right: "25px",
-                    }}
-                  >
-                    <button
-                      style={{
-                        background: "#973c12",
-                        color: "white",
-                        padding: "10px 20px",
-                        borderRadius: "3px",
-                        border: "none",
-                        cursor: "pointer",
-                        margin: "10px 0",
-                      }}
-                      onClick={handleLogout}
-                    >
-                      Sign Out
-                    </button>
-                  </div>
-                  <FaultSubmissionForm />
-                  <FaultList />
-                </>
-              }
-            />
-          )}
-        </Routes>
+        <LoginPage onLogin={handleLogin}/>
+        <ToastContainer position='top-center' />
       </div>
-    </Router>
-  );
+    );
+  } else {
+    return (
+      <div className="container-main" >
+        <div style={{
+          position:'absolute',
+          top:'10px',
+          right:'25px',
+        }}>
+        <button
+          style={{
+            background: '#973c12',
+            color:'white',
+            padding:'10px 20px',
+            borderRadius: '3px',
+            border:'none',
+            cursor:'pointer',
+            margin:'10px 0'
+          }}
+          onClick={handleLogout}
+        >
+          Sign Out
+        </button>
+        </div>
+        <FaultSubmissionForm />
+        <FaultList />
+        <ToastContainer position='top-center' />
+      </div>
+    );
+  }
 }
 
-export default App;
+
+export default App
