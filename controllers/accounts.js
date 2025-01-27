45b03fbb755d6604c025561a416dcf292f32a507
joelcave42 <joelcave1717@gmail.com>
2025-01-26 19:44:39 -0500

    login now cheks for username and password in database

--- controllers/accounts.js
+++ controllers/accounts.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const Account = require('../models/Account');
 
 // Create a new account
@@ -14,6 +15,7 @@ const createAccount = async (req, res) => {
     }
 };
 
+// Create account as a supervisor
 const createAccountAsSupervisor = async (req, res) => {
     try {
         // Temporarily bypass the supervisor check
@@ -33,7 +35,6 @@ const createAccountAsSupervisor = async (req, res) => {
     }
 };
 
-
 // Get all accounts
 const getAccounts = async (req, res) => {
     try {
@@ -44,8 +45,40 @@ const getAccounts = async (req, res) => {
     }
 };
 
+// Login user
+const loginUser = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        // Check if the user exists
+        const user = await Account.findOne({ username });
+        if (!user) {
+            return res.status(404).json({ success: false, message: "User not found" });
+        }
+
+        // Compare passwords
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ success: false, message: "Invalid credentials" });
+        }
+
+        // Successful login
+        res.status(200).json({
+            success: true,
+            message: "Login successful",
+            data: {
+                username: user.username,
+                accountType: user.accountType,
+            },
+        });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
+};
+
 module.exports = {
     createAccount,
     createAccountAsSupervisor,
     getAccounts,
+    loginUser,
 };
